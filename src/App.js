e86df1e3fbdd720a7630f43e0131e7337e711186
Izabayo Blaise <52036624+blaise82@users.noreply.github.com>
2020-04-14 22:23:15 +0200

    ch(setup): Setup an empty react project
    - Initialize the project
    - add src folder
    - add public folder
    - set up webpack
    - set up babel
    - set up prettier
    
    [starts #170947368]

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Barefoot Nomad! change while setting up anything!!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+	mode: 'development',
+	devServer: {
+		historyApiFallback: {
+			disableDotRule: true
+		}
+	},
+	entry: './src/index',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/',
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ 'babel-loader' ]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html'
+		}),
+		new webpack.DefinePlugin({
+			PRODUCTION_API: JSON.stringify('https://octopus-bn-backend-staging.herokuapp.com/api/v1/')
+		}),
+		new webpack.DefinePlugin({
+			DEVELOPMENT_API: JSON.stringify('http://localhost:3000/api/v1/')
+		})
+	]
+};
