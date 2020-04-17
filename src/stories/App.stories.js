5e77aa7922a792defc35f4c63b0aea66c8ba2697
Abdoul <abdoulniyigena@gmail.com>
2020-04-17 11:31:00 +0200

    chore(storybook): configure storybook
    - install all storybook dependencies
    - add the storybook script to package.json
    
    [Maintains #170947377]

---
+++ src/stories/App.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Welcome } from "@storybook/react/demo";
+import App from "../App";
+
+export default {
+  title: "Welcome",
+};
+
+export const app = () => <App />;
