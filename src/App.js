1536a5efb4a69cb7084e42a2eb6186d53c21363c
Emmanuel HAKORIMANA <hakoemmy.2017@gmail.com>
2020-04-18 11:16:27 +0200

    chore(react-router): setup react router
    - add react-router module
    - add react-router-dom module
    
    -[Maintains #170947372]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React, {Component} from 'react';
 import { Provider } from 'react-redux';
 import Counter from '../src/components/Counter';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import store from './redux/store';
+
+const Home = () => <h1> Dummy Home Page</h1>;
+const Login = () => <h1> Dummy Login Page </h1>;
 
 class App extends Component {
 	render(){
 		return (
 	 <Provider store = {store}>
-         <div className="center">
-             <Counter counter/>
-          </div>
+        <Router>
+      <div className="center">
+             <Switch>
+             <Route path='/' exact component={Home} />
+             <Route path='/signin' exact component={Login}/>
+             <Route path='/count' exact component={Counter}/>
+             </Switch>
+      </div> 
+      </Router>
      </Provider>
 	  );
-	}
-
+  }
 }
 
 export default App;
